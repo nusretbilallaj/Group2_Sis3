188a64feca0f161655a1e043bf1e30dd07a59eba
Nusret Bilallaj <netktit@gmail.com>
2022-12-01 18:59:20 +0100

    Shtimi i operacioneve tjera

--- Group2_Sis3/Controllers/StudentController.cs
+++ Group2_Sis3/Controllers/StudentController.cs
@@ -14,18 +14,91 @@ namespace Group2_Sis3.Controllers
         }
         public IActionResult Listo()
         {
+           
             List<Studenti> lista = _konteksti.Studentet.ToList();
             return View(lista);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edito(Studenti stud)
+        {
+            if (stud.KomunaId == 0)
+            {
+                ModelState.AddModelError("KomunaId", "Specifiko komunen");
+            }
+            if (ModelState.IsValid)
+            {
+                _konteksti.Studentet.Update(stud);
+                _konteksti.SaveChanges();
+                return RedirectToAction("Listo");
+            }
+
+            ViewBag.Kom = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
+            {
+                Text = kom.Emri,
+                Value = kom.Id.ToString()
+            });
+            return View(stud);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Fshi(Studenti stud)
+        {
+
+                _konteksti.Studentet.Remove(stud);
+                _konteksti.SaveChanges();
+                return RedirectToAction("Listo");
+        }
+        public IActionResult Fshi(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Studenti studentDb = _konteksti.Studentet.Find(id);
+            if (studentDb == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Kom = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
+            {
+                Text = kom.Emri,
+                Value = kom.Id.ToString()
+            });
+            return View(studentDb);
+        }
+        public IActionResult Edito(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+
+            Studenti studentDb = _konteksti.Studentet.Find(id);
+            if (studentDb==null)
+            {
+                return NotFound();
+            }
+            ViewBag.Kom = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
+            {
+                Text = kom.Emri,
+                Value = kom.Id.ToString()
+            });
+            return View(studentDb);
+        }
         public IActionResult Krijo()
         {
-            Studenti stu = new Studenti();
+            // Studenti stu = new();
+            Studenti stu2 = new Studenti();
+            // var stud3 = new Studenti();
             ViewBag.Komunat = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
             {
                 Text = kom.Emri,
                 Value = kom.Id.ToString()
             });
-            return View(stu);
+            return View(stu2);
         }
         [HttpPost]
         public IActionResult Krijo(Studenti stud)
@@ -40,15 +113,14 @@ namespace Group2_Sis3.Controllers
                 _konteksti.SaveChanges();
                 return RedirectToAction("Listo");
             }
-            else
+
+            ViewBag.Komunat = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
             {
-                ViewBag.Komunat = _konteksti.Komunat.ToList().Select(kom => new SelectListItem()
-                {
-                    Text = kom.Emri,
-                    Value = kom.Id.ToString()
-                });
-                return View(stud);
-            }
+                Text = kom.Emri,
+                Value = kom.Id.ToString()
+            });
+            return View(stud);
+
         }
     }
 }
